da2eed96e6b256e1f48ffdcb740889379b76a43b
Kevin Ziechmann <kziechmann@gmail.com>
2020-01-19 13:50:03 -0700

    Redux, thunk setup to call coincap api GET/assets displaying data in table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import 'babel-polyfill'
 import React from 'react';
 import HomePage from './views/pages/HomePage'
+import { Provider } from 'react-redux'
+import appStore from './data/redux/appStore'
 import './App.css';
 
+const store = appStore;
+
 function App() {
+
   return (
-    <div className="App">
-        <HomePage/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+          <HomePage/>
+      </div>
+    </Provider>
   );
 }
 
